31a065de7b916a43e86e6e36f9a6eaad1213324c
Montather Handal <ali.louy2005@gmail.com>
2021-05-22 17:07:58 -0500

    New Proj Struc

--- BulkyBook_Project/Controllers/HomeController.cs
+++ BulkyBook_Project/Controllers/HomeController.cs
@@ -29,6 +29,8 @@ namespace BulkyBook_Project.Controllers
             return View();
         }
 
+
+
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
