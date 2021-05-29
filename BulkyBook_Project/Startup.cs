4670ec424f1d74992474cfc3e9e1a03a5032fe45
Montather Handal <ali.louy2005@gmail.com>
2021-05-29 16:00:35 -0500

    Migration Works

--- BulkyBook_Project/Startup.cs
+++ BulkyBook_Project/Startup.cs
@@ -28,9 +28,11 @@ namespace BulkyBook_Project
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddDbContext<ApplicationDbContext>(options =>
-                options.UseSqlite(
-                    Configuration.GetConnectionString("DefaultConnection")));
+            //services.AddDbContext<ApplicationDbContext>(options =>
+            //    options.UseSqlite(
+            //        Configuration.GetConnectionString("DefaultConnection")));
+            services.AddDbContext<ApplicationDbContext>(item => item.UseMySql(Configuration.GetConnectionString("DefaultConnection")));
+
             services.AddDatabaseDeveloperPageExceptionFilter();
 
             services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
